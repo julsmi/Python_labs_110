fa5344a82283ca6c1db406f8f74f9bc1eb9be39c
julsmi <115922369+julsmi@users.noreply.github.com>
2022-12-21 09:28:38 +0300

    Лабораторная работа_4

---
+++ task1_1.py
@@ -0,0 +1,18 @@
+import json
+import re
+
+BOOKS_FILE = "books.md"
+BOOK_REGEX = r"""####\s+(?P<position>\d+)\.\s+\[(?P<book>.+?)\]\((?P<book_url>.+?)\)\s+by\s+(?P<author>.+?)\s+\((?P<recommended>\d{1,3}\.\d+%)\s+recommended\)\s+!\[.*?\]\((?P<cover_url>.+?)\)\s+\"(?P<description>.+?)\"\s+\[.*?\]\(.*?\)"""
+
+
+
+def task():
+    book_pattern = re.compile(BOOK_REGEX, re.DOTALL)  # флаг re.DOTALL описывает, что под символом точка может содержаться символ переноса строки
+
+    with open(BOOKS_FILE) as f:
+        for book in book_pattern.finditer(f.read()):
+            print(json.dumps(book.groupdict(), indent=4))
+
+
+if __name__ == '__main__':
+    task()
